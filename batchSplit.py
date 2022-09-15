24e5700cc96357497d7df5ee68ba47c6a0a7b4fd
Matías Alexander Hillmann <mailmatiash@gmail.com>
2022-09-14 21:13:23 -0300

    Update batchSplit.py

--- batchSplit.py
+++ batchSplit.py
@@ -8,15 +8,15 @@ img_number = 1
 
 
 for root, dirs, files in os.walk("batch"):
-    for name in files:    
-        print (os.path.join(root, name)) # will print path of files 
-        path = os.path.join(root, name)
-        im = Image.open(path)
-	#tomar medidas de la imagen dinámicamente
-	x1 = 1080
-	y1 = 820
-	x2 = 3000
-	y2 = 1790
-        cropped = im.crop((x1,y1,x1/2,y2))
-        cropped.save("batchprocessed/"+str(img_number)+".png", "PNG")
-        img_number +=1
+	for name in files:    
+		print (os.path.join(root, name)) # will print path of files 
+		path = os.path.join(root, name)
+		im = Image.open(path)
+		#tomar medidas de la imagen dinámicamente
+		x1 = 1080
+		y1 = 820
+		x2 = 3000
+		y2 = 1790
+		cropped = im.crop((x1,y1,x1/2,y2))
+		cropped.save("batchprocessed/"+str(img_number)+".png", "PNG")
+		img_number +=1
